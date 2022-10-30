4103d3148b9fad47c7372c607796bb7d4d3314ff
Eryk519 <74097448+Eryk519@users.noreply.github.com>
2022-10-30 11:14:23 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,33 @@
-from PIL import Image  # Python Imaging Library
+from PIL import Image
 import numpy as np
 
-print("Zad 2")
-obrazek = Image.open("inicjaly.bmp")
-obrazek.show()
-print("---------- informacje o obrazie")
-print("tryb:", obrazek.mode)
-print("format:", obrazek.format)
-print("rozmiar:", obrazek.size)
 
-print("Zad 3 - zapisanie do pliku tekstowego")
-obrazek_dane = np.asarray(obrazek)
-dane_int = obrazek_dane * 1
-t2_text = open('inicjaly.txt', 'w')
-for rows in dane_int:
-    for item in rows:
-        t2_text.write(str(item) + ' ')
-    t2_text.write('\n')
+print("Zadanie 1")
+obraz= Image.open("inicjaly.bmp")
+obraz.show()
+print("tryb", obraz.mode)
+print("format", obraz.format)
+print("rozmiar", obraz.size)
 
-t2_text.close()
-
-print("Zad 4")
-obrazek_dane = np.asarray(obrazek)
-print("Informacje o tablicy obrazka:")
-print("typ danych tablicy:", obrazek_dane.dtype)
-print("rozmiar tablicy:", obrazek_dane.shape)
-print("liczba elementow:", obrazek_dane.size)
-print("wymiar tablicy:", obrazek_dane.ndim)
-print("rozmiar wyrazu tablicy:",
-      obrazek_dane.itemsize)
-print("pierwszy wyraz:", obrazek_dane[30][50])
-print("drugi wyraz:", obrazek_dane[40][90])
-print("trzeci wyraz:", obrazek_dane[0][99])
+t_obraz = np.asarray(obraz)
+print("typ danych tablicy", t_obraz.dtype)
+print("rozmiar tablicy", t_obraz.shape)
 print("***************************************")
-print(obrazek_dane)
 
-t1 = np.loadtxt("inicjaly.txt", dtype=np.bool_)
-t2 = np.loadtxt("inicjaly.txt", dtype=np.int_)
 
-print("Zad 5")
-print("typ danych tablicy t1:", t1.dtype)
-print("rozmiar tablicy t1 :", t1.shape)
-print("wymiar tablicy t1 :", t1.ndim)
+def wstaw_obraz(t_obraz,wsp,h_m,w_m):
+    h0, w0 = t_obraz.shape
+    print(h0, w0)
+    t = (int(wsp * h0), int(wsp * w0))
+    tab = np.zeros(t, dtype=np.uint8)
+    for i in range(int(h_m), int(h_m) + h0):
+        for j in range(int(w_m), int(w_m) + w0):
+            tab[i][j] = t_obraz[i - int(h_m)][j - int(w_m)]
+    tab = tab.astype(bool)
+    obraz_wstawiany = Image.fromarray(tab)
+    return obraz_wstawiany
 
-print("typ danych tablicy t2:", t2.dtype)
-print("rozmiar tablicy t2 :", t2.shape)
-print("wymiar tablicy t2 :", t2.ndim)
+obraz_wstawiany = wstaw_obraz(t_obraz,2,60,70)
+obraz_wstawiany.show()
 
-print("Zad 6")
-nowa_t2 = np.loadtxt("inicjaly.txt", dtype=np.int_)
-print("--- nowa_t2 -----------")
-print(nowa_t2)
-print("----- t2 ---------")
-print(t2)
-porownanie2 = t2 == nowa_t2
-print("Porownanie tablicy")
-print(porownanie2)
 
-print("Zad 6 dalej")
-ob_i1 = Image.fromarray(t2)
-print("tryb:", ob_i1.mode)
-print("format:", ob_i1.format)
-print("rozmiar:", ob_i1.size)
-ob_i1.show()
